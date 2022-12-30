68b23be0b6961f1bbb00cb04660dafe972ae64c4
s27081 <115358253+s27081@users.noreply.github.com>
2022-12-30 16:20:34 +0100

    Add files via upload

---
+++ zad 4.3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int dataArr[10][10];
+
+	for (int i = 0; i < 10; i++) {
+			dataArr[i][0] = i;
+			dataArr[i][1] = i + i;
+			dataArr[i][2] = i * i;
+			dataArr[i][3] = i + 3;
+			dataArr[i][4] = i - 4;
+			for (int h = 5; h < 10; h++) {
+				dataArr[i][h] = 0;
+			}
+	}
+
+	for (int k = 0; k < 10; k++) {
+		for (int l = 0; l < 10; l++) {
+			cout << dataArr[k][l] << " ";
+		}
+		cout << endl;
+	}
+}
+
